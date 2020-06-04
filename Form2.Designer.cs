49029c80ec854944339d5d4ec47d16fc0902a4fa
TsykuraWork <52891026+TsykuraWork@users.noreply.github.com>
2020-06-04 23:42:48 +0300

    Прописать вкладки Gayme и хелп

---
+++ Form2.Designer.cs
@@ -0,0 +1,20 @@
+﻿
+            this.играToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьЗановоToolStripMenuItem,
+            this.играНаВремяToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.играToolStripMenuItem.Font = new System.Drawing.Font("Sans-serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.играToolStripMenuItem.Text = "gayme";
+			
+			
+			
+			this.помошьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стратегииToolStripMenuItem,
+            this.правилаИгрыToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.помошьToolStripMenuItem.Font = new System.Drawing.Font("Sans-serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.помошьToolStripMenuItem.Name = "помошьToolStripMenuItem";
+            this.помошьToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.помошьToolStripMenuItem.Text = "Help ";
