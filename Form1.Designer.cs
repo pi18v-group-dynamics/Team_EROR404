0681020e4d1a425c59549e9f7e4e642341254269
TsykuraWork <52891026+TsykuraWork@users.noreply.github.com>
2020-06-04 23:55:02 +0300

    Изменить название форм и иконку

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,1 +1,2 @@
-﻿this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+﻿this.Text = "Игра \"Крестики-нолики\"";
+this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
