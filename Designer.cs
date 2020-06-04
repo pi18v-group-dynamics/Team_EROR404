4d138a558a11332d0d3bf8624fcdb4b798371aa1
TsykuraWork <52891026+TsykuraWork@users.noreply.github.com>
2020-06-04 23:29:28 +0300

    Прописать вкладки Gayme и хелп

--- Designer.cs
+++ Designer.cs
@@ -1,102 +1,19 @@
-namespace WindowsFormsApplication2
-{
-    partial class Form2
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(30, 43);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(335, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = " ÚÓ ÔÂ‚˚È ıÓ‰ËÚ";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
-            this.button1.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(25, 85);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 31);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "’";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
-            this.button2.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(200, 91);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 35);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "0";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // Form2
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Teal;
-
-            this.ClientSize = new System.Drawing.Size(519, 237);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Form2";
-            this.Load += new System.EventHandler(this.Form2_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-    }
-}
+this.Ë„‡ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Ë„‡Ú¸«‡ÌÓ‚ÓToolStripMenuItem,
+            this.Ë„‡Õ‡¬ÂÏˇToolStripMenuItem,
+            this.‚˚ıÓ‰ToolStripMenuItem});
+            this.Ë„‡ToolStripMenuItem.Font = new System.Drawing.Font("Sans-serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Ë„‡ToolStripMenuItem.Name = "Ë„‡ToolStripMenuItem";
+            this.Ë„‡ToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.Ë„‡ToolStripMenuItem.Text = "gayme";
+			
+			
+			
+			this.ÔÓÏÓ¯¸ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ÒÚ‡ÚÂ„ËËToolStripMenuItem,
+            this.Ô‡‚ËÎ‡»„˚ToolStripMenuItem,
+            this.ÓœÓ„‡ÏÏÂToolStripMenuItem});
+            this.ÔÓÏÓ¯¸ToolStripMenuItem.Font = new System.Drawing.Font("Sans-serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ÔÓÏÓ¯¸ToolStripMenuItem.Name = "ÔÓÏÓ¯¸ToolStripMenuItem";
+            this.ÔÓÏÓ¯¸ToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.ÔÓÏÓ¯¸ToolStripMenuItem.Text = "Help ";
