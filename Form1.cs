fbd5e5ef63d48edada1492498f3ce2829dcc4362
FrolovWork <60880425+FrolovWork@users.noreply.github.com>
2020-06-05 00:15:49 +0300

    Графический интерфейс

--- Form1.cs
+++ Form1.cs
@@ -1,113 +1,135 @@
-﻿ if (!indicator)
-                    {
-                   
-                    MessageBox.Show("На этот раз победили Крестики");
-                   
+﻿if (!indicator)
+{
 
-                }
-                    else
-                    {
-                 
-                    MessageBox.Show("Победу в этом сражении одержали Нолики");
+MessageBox.Show("На этот раз победили Крестики");
+
+
+}
+else
+{
+
+MessageBox.Show("Победу в этом сражении одержали Нолики");
 namespace WindowsFormsApplication2
 {
-    partial class Form2
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+partial class Form2
+{
+/// <summary>
+/// Required designer variable.
+/// </summary>
+private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+/// <summary>
+/// Clean up any resources being used.
+/// </summary>
+/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+protected override void Dispose(bool disposing)
+{
+if (disposing && (components != null))
+{
+components.Dispose();
+}
+base.Dispose(disposing);
+}
 
-        #region Windows Form Designer generated code
+#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(30, 43);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(335, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Êòî ïåðâûé õîäèò";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
-            this.button1.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(25, 85);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 31);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Õ";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
-            this.button2.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(200, 91);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 35);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "0";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // Form2
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Teal;
+/// <summary>
+/// Required method for Designer support - do not modify
+/// the contents of this method with the code editor.
+/// </summary>
+private void InitializeComponent()
+{
+System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+this.label1 = new System.Windows.Forms.Label();
+this.button1 = new System.Windows.Forms.Button();
+this.button2 = new System.Windows.Forms.Button();
+this.SuspendLayout();
+//
+// label1
+//
+this.label1.AutoSize = true;
+this.label1.BackColor = System.Drawing.Color.Transparent;
+this.label1.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+this.label1.Location = new System.Drawing.Point(30, 43);
+this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+this.label1.Name = "label1";
+this.label1.Size = new System.Drawing.Size(335, 32);
+this.label1.TabIndex = 0;
+this.label1.Text = "Êòî ïåðâûé õîäèò";
+this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+this.label1.Click += new System.EventHandler(this.label1_Click);
+//
+// button1
+//
+this.button1.BackColor = System.Drawing.Color.Transparent;
+this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
+this.button1.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+this.button1.Location = new System.Drawing.Point(25, 85);
+this.button1.Margin = new System.Windows.Forms.Padding(4);
+this.button1.Name = "button1";
+this.button1.Size = new System.Drawing.Size(150, 31);
+this.button1.TabIndex = 1;
+this.button1.Text = "Õ";
+this.button1.UseVisualStyleBackColor = false;
+//
+// button2
+//
+this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
+this.button2.Font = new System.Drawing.Font("Sans-serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+this.button2.Location = new System.Drawing.Point(200, 91);
+this.button2.Margin = new System.Windows.Forms.Padding(4);
+this.button2.Name = "button2";
+this.button2.Size = new System.Drawing.Size(115, 35);
+this.button2.TabIndex = 2;
+this.button2.Text = "0";
+this.button2.UseVisualStyleBackColor = true;
+//
+// Form2
+//
+this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+this.BackColor = System.Drawing.Color.Teal;
 
-            this.ClientSize = new System.Drawing.Size(519, 237);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Form2";
-            this.Load += new System.EventHandler(this.Form2_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+this.ClientSize = new System.Drawing.Size(519, 237);
+this.Controls.Add(this.button2);
+this.Controls.Add(this.button1);
+this.Controls.Add(this.label1);
+this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+this.Margin = new System.Windows.Forms.Padding(4);
+this.Name = "Form2";
+this.Load += new System.EventHandler(this.Form2_Load);
+this.ResumeLayout(false);
+this.PerformLayout();
 
-        }
+}
 
-        #endregion
+#endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-    }
-} 
+private System.Windows.Forms.Label label1;
+private System.Windows.Forms.Button button1;
+private System.Windows.Forms.Button button2;
+}
+}
+
+namespace WindowsFormsApplication2
+{
+partial class Form1
+{
+/// <summary>
+/// Требуется переменная конструктора.
+/// </summary>
+private System.ComponentModel.IContainer components = null;
+
+/// <summary>
+/// Освободить все используемые ресурсы.
+/// </summary>
+/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+protected override void Dispose(bool disposing)
+{
+if (disposing && (components != null))
+{
+components.Dispose();
+}
+base.Dispose(disposing);
+}
